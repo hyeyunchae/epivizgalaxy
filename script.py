0a5fe3ad6107715a31fe186179b209a0cf3f20f9
Hyeyun Chae <chaehy415@gmail.com>
2019-07-01 16:26:16 -0400

    updated getData and script

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
-from parser import BigWig
+from parser import create_parser_object
 import os
 import numpy
 import pickle
 import sys
 
-ff = BigWig(sys.argv[1])
-res, _ = ff.getRange("chr1", 1, 100, 2)
+ff = create_parser_object(sys.argv[2], sys.argv[1])
+res, _ = ff.getRange(sys.argv[3],int(sys.argv[4]),int(sys.argv[5]))
 print(res)
